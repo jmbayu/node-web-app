650304406c39da89d313a3b06566651814109ac6
jmbayu <me@example.com>
2022-03-03 22:20:34 +0000

    add timestamp

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const express = require('express');
 const PORT = process.env.PORT || "8080";
 const HOST = process.env.HOST || "0.0.0.0";
 const VERSION = process.env.VERSION || "0.0";
-const myObject={ id: 1, message: "Hello", sender:"Alice", receiver: "Bob"}
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   // res.send(`Hello World  VERSION ${VERSION}`);
-  res.send(myObject);
+  var timeStamp = Math.floor(new Date().getTime() / 1000)
+  var responseObject={ id: 1, message: "Hello", sender:"Alice", receiver: "Bob", timeStamp}
+  res.send(responseObject);
 
 });
 
 app.listen(PORT, HOST);
-console.log(myObject);
 console.log(`Running on http://${HOST}:${PORT}`);
