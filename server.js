1cd310a2f274f7b2d57d9e2c493b4e883ede11ae
jmbayu <me@example.com>
2022-03-03 20:30:03 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+
+
+// Constants
+const PORT = process.env.PORT || "8080";
+const HOST = process.env.HOST || "0.0.0.0";
+const VERSION = process.env.VERSION || "0.0";
+const myObject={ message: "Hello", sender:"Alice", receiver: "Bob"}
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  // res.send(`Hello World  VERSION ${VERSION}`);
+  res.send(myObject);
+
+});
+
+app.listen(PORT, HOST);
+console.log(myObject);
+console.log(`Running on http://${HOST}:${PORT}`);
