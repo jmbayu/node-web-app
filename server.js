c69d9f68e6ca4abe7af4b24eaf995dd55fa51e2a
jmbayu <me@example.com>
2022-03-04 09:31:58 +0000

    add config

--- server.js
+++ server.js
@@ -1,19 +1,50 @@
 'use strict';
 
 const express = require('express');
+const fs = require("fs");
 
 // Constants
 const PORT = process.env.PORT || "8080";
 const HOST = process.env.HOST || "0.0.0.0";
 const VERSION = process.env.VERSION || "0.0";
 
+// Functions
+function jsonReader(filePath, cb) {
+  fs.readFile(filePath, (err, fileData) => {
+    if (err) {
+      return cb && cb(err);
+    }
+    try {
+      const object = JSON.parse(fileData);
+      return cb && cb(null, object);
+    } catch (err) {
+      return cb && cb(err);
+    }
+  });
+}
+
 // App
 const app = express();
 app.get('/', (req, res) => {
 
-  var timeStamp = Math.floor(new Date().getTime() / 1000)
-  var responseObject={ id: 1, message: "Hello", sender:"Alice", receiver: "Bob", timeStamp}
-  res.send(responseObject);
+  jsonReader("./config.json", (err, customer) => {
+    if (err) {
+      console.log("Error reading file:", err);
+      return;
+    }
+    // increase customer order count by 1
+    config.run_count += 1;
+    rc=config.run_count;
+    //
+    var timeStamp = Math.floor(new Date().getTime() / 1000)
+    var responseObject={ rc, message: "Hello", sender:"Alice", receiver: "Bob", timeStamp}
+    res.send(responseObject);
+
+    fs.writeFile("./config.json", JSON.stringify(customer), err => {
+      if (err) console.log("Error writing file:", err);
+    });
+  });
+
 
 });
 
